f6e3ed680cae29c52c517954dfcd2f2cd4352f7f
Zhe Wang <akina.dwnhl@gmail.com>
2016-08-30 09:20:42 -0700

    setup bunlde/npm install

---
+++ db/migrate/20160830160544_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.integer :id, null: false
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :email, null: false
+      t.integer :age, null: false
+      t.string :gender
+      t.string :location
+
+      t.timestamps null: false
+    end
+
+    add_index :users, [:username, :email], unique: true
+  end
+end
