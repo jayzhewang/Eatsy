43b7af7ebf4591449d2e487b1da9b6207d9bb2b8
Zhe Wang <akina.dwnhl@gmail.com>
2016-08-30 11:01:31 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :users, only: [:new, :create], defaults: { format: :json }
+    resource :session, only: [:create, :destroy]
   end
 
   root 'static_pages#root'
