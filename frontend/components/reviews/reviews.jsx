edd8a2152860134dee9e242b7c21ab8b3b8c8d58
Zhe Wang <akina.dwnhl@gmail.com>
2016-10-17 13:01:51 -0700

    css

--- frontend/components/reviews/reviews.jsx
+++ frontend/components/reviews/reviews.jsx
@@ -185,7 +185,7 @@ class Reviews extends React.Component {
                                              starCount={5}
                                              value={review.rating}
                                              starColor={'#e5050b'}
-                                             emptyStarColor={'#acacacbp'}
+                                             emptyStarColor={'#acacac'}
                                              editing={false}
                                              renderStarIcon={()=>
                                                <span className='star-symbol'>
