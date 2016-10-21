9086aa23651f8ff5bb56c079fe628e9e83e5c4c0
Zhe Wang <akina.dwnhl@gmail.com>
2016-10-21 12:59:55 -0700

    added webpack production ENV

--- webpack.config.js
+++ webpack.config.js
@@ -26,5 +26,12 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new webpack.DefinePlugin({
+        'process.env': {
+            'NODE_ENV': '"production"'
+        }
+    })
+  ],
   devtool: 'source-maps'
 };
